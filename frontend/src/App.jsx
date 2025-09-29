c3bf0e5dbf6ee81ea157b8cde8fb4d752bdc1fa4
christianyitzak <christianyitzak@gmail.com>
2025-09-29 09:58:16 +0700

    seperate into smaller components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
-import axios from 'axios'
-import { Routes, Route } from 'react-router'
-import { useEffect, useState } from 'react'
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/checkout/CheckoutPage'
-import { OrdersPage } from './pages/OrdersPage'
-import { TrackingPage } from './pages/TrackingPage'
-import { ErrorPage } from './pages/ErrorPage'
+import axios from "axios";
+import { Routes, Route } from "react-router";
+import { useEffect, useState } from "react";
+import { HomePage } from "./pages/home/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
-	const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get('api/cart-items?expand=product')
-			.then((response) => setCart(response.data))
-	}, [])
+    useEffect(() => {
+        axios.get("api/cart-items?expand=product").then((response) => setCart(response.data));
+    }, []);
 
-	return (
-		<Routes>
-			<Route index element={<HomePage cart={cart} />} />
-			<Route path='checkout' element={<CheckoutPage cart={cart} />} />
-			<Route path='orders' element={<OrdersPage cart={cart} />} />
-			<Route path='tracking' element={<TrackingPage />} />
-			<Route path='*' element={<ErrorPage />} />
-		</Routes>
-	)
+    return (
+        <Routes>
+            <Route index element={<HomePage cart={cart} />} />
+            <Route path='checkout' element={<CheckoutPage cart={cart} />} />
+            <Route path='orders' element={<OrdersPage cart={cart} />} />
+            <Route path='tracking' element={<TrackingPage />} />
+            <Route path='*' element={<ErrorPage />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
