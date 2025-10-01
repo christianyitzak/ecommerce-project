865fa7f5f582f351bde4ff9357cc7baae1f89d01
christianyitzak <christianyitzak@gmail.com>
2025-10-01 21:52:00 +0700

    create integration test for product component

--- frontend/src/pages/home/Product.jsx
+++ frontend/src/pages/home/Product.jsx
@@ -29,7 +29,7 @@ export function Product({ product, loadCart }) {
     return (
         <div className='product-container'>
             <div className='product-image-container'>
-                <img className='product-image' src={product.image} />
+                <img className='product-image' src={product.image} data-testid='product-image' />
             </div>
 
             <div className='product-name limit-text-to-2-lines'>{product.name}</div>
@@ -37,6 +37,7 @@ export function Product({ product, loadCart }) {
             <div className='product-rating-container'>
                 <img
                     className='product-rating-stars'
+                    data-testid='product-rating-stars'
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`}
                 />
                 <div className='product-rating-count link-primary'>{product.rating.count}</div>
